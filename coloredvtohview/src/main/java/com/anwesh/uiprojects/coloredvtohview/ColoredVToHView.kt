abb8f6a5621e81663ab4d9ff407a68f0fc7557c7
Anwesh43 <anweshthecool0@gmail.com>
2020-05-30 07:10:39 +0530

    adding constants and libraries

--- coloredvtohview/src/main/java/com/anwesh/uiprojects/coloredvtohview/ColoredVToHView.kt
+++ coloredvtohview/src/main/java/com/anwesh/uiprojects/coloredvtohview/ColoredVToHView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.coloredvtohview
 /**
  * Created by anweshmishra on 30/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val parts : Int = 3
+val colors : Array<String> = arrayOf("#4CAF50", "#2196F3", "#F44336", "#009688", "#00BCD4")
+val scGap : Float = 0.02f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val sizeFactor : Float = 2.9f
