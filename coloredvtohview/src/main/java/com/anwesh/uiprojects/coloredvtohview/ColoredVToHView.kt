16c32cecac462fb67ec7ef5bb034064c76112586
Anwesh43 <anweshthecool0@gmail.com>
2020-05-30 07:35:49 +0530

    adding CVTHNode class

--- coloredvtohview/src/main/java/com/anwesh/uiprojects/coloredvtohview/ColoredVToHView.kt
+++ coloredvtohview/src/main/java/com/anwesh/uiprojects/coloredvtohview/ColoredVToHView.kt
@@ -123,4 +123,45 @@ class ColoredVToHView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CVTHNode(var i : Int, val state : State = State()) {
+
+        private var next : CVTHNode? = null
+        private var prev : CVTHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CVTHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCVTHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CVTHNode {
+            var curr : CVTHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
